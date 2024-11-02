0d574a6080b071131565db805782bdc8a4e472a5
kooner27 <prabhnoo@ualberta.ca>
2024-11-02 17:19:59 -0600

    commenting

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <unordered_map>
-
 using namespace std;
-
 int main() {
-    // clangd file in ~ directoroy -std = c++20 for latest lsp
-    unordered_map<int, int> map;
-    map.insert({5, 5});
-    cout << map.contains(5) << endl;
+    std::unordered_map<int, int> map;
+    map.insert({5, 3});
+    cout << map[5] << endl;
     return 0;
 }
