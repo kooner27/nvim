82a8de1dc1621820b546baa4c5866ed07442c07d
kooner27 <prabhnoo@ualberta.ca>
2024-11-26 20:42:33 -0700

    emmet lsp for html tags

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -184,12 +184,29 @@ return {
 				-- gopls = {},
 				-- pyright = {},
 				rust_analyzer = {},
+				bashls = {},
+				lua_ls = {
+					-- cmd = {...},
+					-- filetypes = { ...},
+					-- capabilities = {},
+					settings = {
+						Lua = {
+							completion = {
+								callSnippet = "Replace",
+							},
+							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+							-- diagnostics = { disable = { 'missing-fields' } },
+						},
+					},
+				},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`ts_ls`) will work just fine
+
+				------ web stuff
 				ts_ls = {
 					settings = {
 						completions = {
@@ -199,9 +216,10 @@ return {
 					},
 					filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
 				},
+				-----
 				html = {},
-				bashls = {},
-				-----------------
+				emmet_language_server = {},
+				-----
 				cssls = {
 					settings = {
 						css = {
@@ -227,6 +245,7 @@ return {
 					},
 					filetypes = { "css", "scss", "less" }, -- Ensure these filetypes are supported
 				},
+				----
 				tailwindcss = {
 					filetypes = {
 						"css",
@@ -264,22 +283,7 @@ return {
 						},
 					},
 				},
-
 				-----------------
-				lua_ls = {
-					-- cmd = {...},
-					-- filetypes = { ...},
-					-- capabilities = {},
-					settings = {
-						Lua = {
-							completion = {
-								callSnippet = "Replace",
-							},
-							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-							-- diagnostics = { disable = { 'missing-fields' } },
-						},
-					},
-				},
 			}
 
 			-- Ensure the servers and tools above are installed
