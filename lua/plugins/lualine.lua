50038184d18466806357d3422c9164b683cbd91c
kooner27 <prabhnoo@ualberta.ca>
2024-12-12 18:23:44 -0700

    dont lint html with stylelint

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,36 +1,3 @@
--- return {
--- 	"nvim-lualine/lualine.nvim",
--- 	dependencies = { "nvim-tree/nvim-web-devicons" },
--- 	lazy = false,
--- 	opts = function(_, opts)
--- 		-- Set up Dracula theme and icons globally
--- 		opts.options = opts.options or {}
--- 		opts.options.theme = "dracula"
--- 		opts.options.icons_enabled = true
---
--- 		-- Configure trouble symbols for lualine
--- 		local trouble = require("trouble")
--- 		local symbols = trouble.statusline({
--- 			mode = "lsp_document_symbols",
--- 			groups = {},
--- 			title = false,
--- 			filter = { range = true },
--- 			format = "{kind_icon}{symbol.name:Normal}",
--- 			hl_group = "lualine_c_normal",
--- 		})
---
--- 		-- Ensure lualine_c section exists and add trouble symbols
--- 		opts.sections = opts.sections or {}
--- 		opts.sections.lualine_c = opts.sections.lualine_c or {}
---
--- 		-- Insert trouble symbols into lualine_c section
--- 		table.insert(opts.sections.lualine_c, {
--- 			symbols.get,
--- 			cond = symbols.has,
--- 		})
--- 	end,
--- }
-
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -45,6 +12,8 @@ return {
       hl_group = "lualine_c_normal",
     })
 
+    -- enable global status lualine
+
     -- Initialize opts.sections and opts.sections.lualine_c if not defined
     opts.sections = opts.sections or {}
     opts.sections.lualine_c = opts.sections.lualine_c or {}
@@ -58,15 +27,15 @@ return {
 }
 
 -- return {
--- 	"nvim-lualine/lualine.nvim",
--- 	dependencies = { "nvim-tree/nvim-web-devicons" },
--- 	config = function()
--- 		require("lualine").setup({
--- 			options = {
--- 				theme = "dracula",
--- 				icons_enabled = true,
--- 			},
--- 		})
--- 	end,
--- 	lazy = false,
+--   "nvim-lualine/lualine.nvim",
+--   dependencies = { "nvim-tree/nvim-web-devicons" },
+--   config = function()
+--     require("lualine").setup({
+--       options = {
+--         theme = "dracula",
+--         icons_enabled = true,
+--       },
+--     })
+--   end,
+--   lazy = false,
 -- }
