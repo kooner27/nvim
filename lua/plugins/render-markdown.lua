2741f90b29b46205af6e0df39626d0f91d4444dc
kooner27 <prabhkooner2727@gmail.com>
2025-11-11 00:23:12 -0700

    cleaner markdown header rendering

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -8,6 +8,28 @@ return {
         clear_in_insert_mode = false, -- keep rendering in insert mode
         render_modes = { 'n', 'i' }, -- render both in normal & insert
         only_render_at_cursor = true, -- only show raw text at cursor line
+        heading = {
+          enabled = true,
+          sign = false,
+          icons = {
+            '# ', -- H1 thin
+            '## ', -- H2
+            '### ', -- H3
+            '#### ', -- H4
+            '##### ', -- H5
+            '###### ', -- H6 full block
+          },
+
+          -- icons = {
+          --   '▏', -- H1 thin
+          --   '▎', -- H2
+          --   '▍', -- H3
+          --   '▌', -- H4
+          --   '▋', -- H5
+          --   '█', -- H6 full block
+          -- },
+          position = 'inline', -- place the icon inline (not overlay)
+        },
       }
     end,
   },
